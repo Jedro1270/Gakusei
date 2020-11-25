4a1560235cf629014fb54f0e7816a2c4e05512b8
Jedro1270 <jedro1270@gmail.com>
2020-11-25 12:55:32 +0800

    feat: add group page body

---
+++ client/src/components/Body/Groups/Groups.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Groups extends Component {
+    render() {
+        return (
+            <div className="Group-page">
+                group stuff here
+            </div>
+        )
+    }
+}
