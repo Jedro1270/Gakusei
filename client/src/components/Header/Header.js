93369015e301b0cfb839bb184792410743db1921
Jedro1270 <jedro1270@gmail.com>
2020-11-25 11:56:42 +0800

    feat: add show menu using hamburger icon

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,10 +3,14 @@ import Burger from './Burger/Burger';
 import Title from './Title/Title';
 
 export default class Header extends Component {
+    toggleMenu() {
+        this.props.toggleMenu()
+    }
+
     render() {
         return (
             <div className="App-header">
-                <button className="Header-burger">
+                <button className="Header-burger" onClick={this.toggleMenu.bind(this)}>
                     <Burger />
                 </button>
                 <div className="Header-title">
