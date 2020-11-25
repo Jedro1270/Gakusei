d2a9c38d00d034bb8b4c56f3841eb34ba7a6c396
Jedro1270 <jedro1270@gmail.com>
2020-11-25 08:34:21 +0800

    feat: create burger component

---
+++ client/src/components/Header/Burger/Burger.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class Burger extends Component {
+    render() {
+        return (
+            <div className="burger">
+                <h1>
+                    &#9776;
+                </h1>
+            </div>
+        )
+    }
+}
