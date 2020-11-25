1b5c7682b64c3538782997e7fc30758bd2718cac
Jedro1270 <jedro1270@gmail.com>
2020-11-25 12:55:54 +0800

    feat: add notebooks page body

---
+++ client/src/components/Body/Notebooks/Notebooks.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Notebooks extends Component {
+    render() {
+        return (
+            <div className="Notebooks-page">
+                notebooks stuff here
+            </div>
+        )
+    }
+}
