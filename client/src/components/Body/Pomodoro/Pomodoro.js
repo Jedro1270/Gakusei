c3d233c9b3380ac8ef574c2ac00fd15081ec4dbf
Jedro1270 <jedro1270@gmail.com>
2020-11-25 12:56:20 +0800

    feat: add pomodoro page body

---
+++ client/src/components/Body/Pomodoro/Pomodoro.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Pomodoro extends Component {
+    render() {
+        return (
+            <div className="Pomodoro-page">
+                Pomodoro stuff here
+            </div>
+        )
+    }
+}
