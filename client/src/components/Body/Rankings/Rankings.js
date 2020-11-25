deb2e39073a2ac505b3a4f7cbb00ecf14db8d8d5
Jedro1270 <jedro1270@gmail.com>
2020-11-25 12:56:41 +0800

    feat: add rankings page body

---
+++ client/src/components/Body/Rankings/Rankings.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Rankings extends Component {
+    render() {
+        return (
+            <div className="Rankings-page">
+                rankings stuff here
+            </div>
+        )
+    }
+}
