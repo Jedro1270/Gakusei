21796ed4e1af4beb3da1f8d95f57b7e15c3f4074
Jedro1270 <jedro1270@gmail.com>
2020-11-26 22:36:18 +0800

    feat: add updateTitle function to change header title

--- client/src/components/Body/Chat/Chat.js
+++ client/src/components/Body/Chat/Chat.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 export default class Chat extends Component {
+    updateTitle(title) {
+        this.props.updateTitle(title)
+    }
+
+    componentDidMount() {
+        this.updateTitle(this.props.title);
+    }
+
     render() {
         return (
             <div className="Chat-page">
--- client/src/components/Body/Notebooks/Notebooks.js
+++ client/src/components/Body/Notebooks/Notebooks.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 export default class Notebooks extends Component {
+    updateTitle(title) {
+        this.props.updateTitle(title)
+    }
+
+    componentDidMount() {
+        this.updateTitle(this.props.title);
+    }
+
     render() {
         return (
             <div className="Notebooks-page">
