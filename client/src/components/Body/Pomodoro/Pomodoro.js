ff24c7f7226a8a51918945a1b1b48d6c6a48bf9d
Jedro1270 <jedro1270@gmail.com>
2020-11-30 11:06:04 +0800

    feat: add pomodoro page layout with placeholders

--- client/src/components/Body/Pomodoro/Pomodoro.js
+++ client/src/components/Body/Pomodoro/Pomodoro.js
@@ -12,7 +12,42 @@ export default class Pomodoro extends Component {
     render() {
         return (
             <div className="Pomodoro-page">
-                Pomodoro stuff here
+                <div className="Pomodoro-members">
+                    <div className="Member">
+                        <div className="Member-status" />
+                        Groupmate Name
+                    </div>
+                    <div className="Member">
+                        <div className="Member-status" />
+                        Groupmate Name
+                    </div>
+                    <div className="Member">
+                        <div className="Member-status" />
+                        Groupmate Name
+                    </div>
+                    <div className="Member">
+                        <div className="Member-status" />
+                        Groupmate Name
+                    </div>
+                </div>
+
+                <div className="Pomodoro-counter">
+                    <h1>
+                        Pomodoros Finished: 5
+                    </h1>
+                </div>
+
+                <div className="Pomodoro-timer">
+                    <h1>
+                        25:00
+                    </h1>
+                </div>
+
+                <div className="Pomodoro-button">
+                    <button>
+                        START
+                    </button>
+                </div>
             </div>
         )
     }
