21d4874c0a3c9825b1c71cfdfd4f0249c282abbc
Jedro1270 <jedro1270@gmail.com>
2020-12-01 09:51:16 +0800

    feat: replace html app bar to material ui

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react';
-import Burger from './Burger/Burger';
 import Title from './Title/Title';
 
+import { AppBar, Drawer, IconButton, styled } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import NavigationMenu from './NavigationMenu/NavigationMenu';
+
 export default class Header extends Component {
-    toggleMenu() {
-        this.props.toggleMenu()
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false,
+        }
+
+        this.openDrawer = this.openDrawer.bind(this);
+        this.closeDrawer = this.closeDrawer.bind(this);
+    }
+
+    openDrawer() {
+        this.setState({ open: true });
+    }
+
+    closeDrawer() {
+        this.setState({ open: false })
     }
 
     render() {
         return (
-            <div className="App-header">
-                <button className="Header-burger" onClick={this.toggleMenu.bind(this)}>
-                    <Burger />
-                </button>
-                <div className="Header-title">
-                    <Title title={this.props.title}/>
-                </div>
-            </div>
+            <TitleBar>
+                <IconButton onClick={this.openDrawer}>
+                    <MenuButton/>
+                </IconButton>
+                <Drawer
+                    anchor='left'
+                    open={this.state.open}
+                    onClose={this.closeDrawer}
+                >
+                    <NavigationMenu/>
+                </Drawer>
+                <Title title={this.props.title}/>
+            </TitleBar>
         )
     }
 }
+
+const TitleBar = styled(AppBar)({
+    position: "static",
+    display: 'flex',
+    flexDirection: 'row',
+    backgroundColor: 'grey',
+});
+
+const MenuButton = styled(MenuIcon) ({
+    color: 'white',
+    fontSize: '50px'
+});
