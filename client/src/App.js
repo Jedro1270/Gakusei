90c1dba4f7b1975c32ef4dbeaa959847373ad031
Jedro1270 <jedro1270@gmail.com>
2020-12-01 09:49:57 +0800

    feat: convert app bar from html elements to material ui

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import {Route, BrowserRouter as Router, Switch} from 'react-router-dom';
 
 import Header from './components/Header/Header';
-import Menu from './components/Header/Menu/Menu';
 import Groups from './components/Body/Groups/Groups';
 import Pomodoro from './components/Body/Pomodoro/Pomodoro';
 import Notebooks from './components/Body/Notebooks/Notebooks';
@@ -11,39 +10,13 @@ import Chat from './components/Body/Chat/Chat';
 import Rankings from './components/Body/Rankings/Rankings';
 
 class App extends Component {
+
   constructor(props) {
     super(props);
     
     this.state = { menuOpen: false, title: "Groups" };
 
-    this.toggleMenu = this.toggleMenu.bind(this);
     this.updateTitle = this.updateTitle.bind(this);
-  }
-
-  callAPI() {
-    fetch("http://localhost:2727/groups") // Change to log/register page in the future
-      .then(response => response.text())
-      .then(response => this.setState({ apiResponse: response }))
-      .catch(error => error)
-  }
-
-  componentDidMount() {
-    this.callAPI();
-  }
-
-  toggleMenu() {
-    if (window.innerWidth < 760) {
-      if (this.state.menuOpen) {
-        document.querySelector('.App-menu').style.transform = "translateX(-100%)";
-      } else {
-        document.querySelector('.App-menu').style.transform = "none";
-      }
-  
-      this.setState(() => ({
-          menuOpen: !this.state.menuOpen
-        })
-      );
-    }
   }
 
   updateTitle(newTitle) {
@@ -54,8 +27,7 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <Header toggleMenu={this.toggleMenu} title={this.state.title}/>
-          <Menu toggleMenu={this.toggleMenu} title={this.state.title}/>
+          <Header title={this.state.title}/>
           <div className="App-body">
             <Switch>
               <Route path="/groups" render={() => (
