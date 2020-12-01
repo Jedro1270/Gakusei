058c368f89cf448a30a7acbeb4fa2e66bd7cb488
Jedro1270 <jedro1270@gmail.com>
2020-12-01 09:52:51 +0800

    feat: replace h1 to typography component

--- client/src/components/Header/Title/Title.js
+++ client/src/components/Header/Title/Title.js
@@ -1,11 +1,22 @@
+import { styled, Typography } from '@material-ui/core';
 import React, { Component } from 'react';
 
 export default class Title extends Component {
     render() {
         return (
-            <h1>
+            <TitleContent>
                 {this.props.title}
-            </h1>
+            </TitleContent>
         );
     }
 }
+
+const TitleContent = styled(Typography)({
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    variant: 'h2',
+    color: 'white',
+    fontWeight: 'bold',
+    padding: '20px',
+    fontSize: '50px'
+});
