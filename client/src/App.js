db2080e89470365978ba9844f7d72f1f8bab7eb1
Jedro1270 <jedro1270@gmail.com>
2020-12-03 14:39:05 +0800

    feat: add subpaths for notebooks and notes

--- client/src/App.js
+++ client/src/App.js
@@ -8,34 +8,45 @@ import Pomodoro from './components/Body/Pomodoro/Pomodoro';
 import Notebooks from './components/Body/Notebooks/Notebooks';
 import Chat from './components/Body/Chat/Chat';
 import Rankings from './components/Body/Rankings/Rankings';
+import Notes from './components/Body/Notebooks/Notes/Notes';
+import NoteContents from './components/Body/Notebooks/Notes/NoteContents/NoteContents';
 
 export default function App() {
   
   const [title, setTitle] = useState('test');
+  const [backButtonNav, setBackButtonNav] = useState(false);
 
   return (
     <Router>
       <div className='App'>
-        <Header title={title}/>
+        <Header title={title} backButtonNav={backButtonNav}/>
         <div className='App-body'>
           <Switch>
-            <Route path='/groups' render={() => (
+            <Route exact path='/groups' render={() => (
                 <Groups setTitle={setTitle}/>
               )} 
             />
-            <Route path='/pomodoro' render={() => (
+            <Route exact path='/pomodoro' render={() => (
                 <Pomodoro setTitle={setTitle}/>
               )} 
             />
-            <Route path='/notebooks' render={() => (
-                <Notebooks setTitle={setTitle}/>
+            <Route exact path='/notebooks' render={() => (
+                <Notebooks setTitle={setTitle} setBackButtonNav={setBackButtonNav}/>
               )} 
             />
-            <Route path='/chat' render={() => (
+            <Route exact path='/notebooks/:notebookTitle' render={() => (
+                <Notes setTitle={setTitle} setBackButtonNav={setBackButtonNav}/>
+              )} 
+            />
+            <Route exact path='/notebooks/:notebookTitle/:noteTitle' render={() => (
+                <NoteContents setTitle={setTitle} setBackButtonNav={setBackButtonNav}/>
+              )} 
+            />
+            <Route exact path='/chat' render={() => (
                 <Chat setTitle={setTitle}/>
               )} 
             />
-            <Route path='/rankings' render={() => (
+            <Route exact path='/rankings' render={() => (
                 <Rankings setTitle={setTitle} title='Rankings and Badges'/>
               )} 
             />
