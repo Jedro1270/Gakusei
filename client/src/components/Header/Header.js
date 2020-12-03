056416f988cf3de8be557d83a20beb2015be6c64
Jedro1270 <jedro1270@gmail.com>
2020-12-03 14:42:40 +0800

    feat: create functionality to switch burger to back button

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,19 +1,39 @@
 import React, { useState } from 'react';
-import Title from './Title/Title';
-
 import { AppBar, Drawer, IconButton, styled } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
+import { Menu, ArrowBack } from '@material-ui/icons';
+import { useHistory } from 'react-router-dom';
+
+import Title from './Title/Title';
 import NavigationMenu from './NavigationMenu/NavigationMenu';
 
 export default function Header(props) {
     
     const [open, setOpen] = useState(false);
+    const history = useHistory();
+
+    const backButtonNav = props.backButtonNav;
+
+    function navigationType() {
+        if (!backButtonNav) {
+            return (
+                <IconButton onClick={() => {setOpen(true)}}>
+                    <MenuButton/>
+                </IconButton>
+            );
+        } else {
+            return (
+                <IconButton onClick={() => {history.goBack()}}>
+                    <BackButton/>
+                </IconButton>
+            );
+        }
+    }
 
     return (
         <TitleBar>
-            <IconButton onClick={() => {setOpen(true)}}>
-                <MenuButton/>
-            </IconButton>
+
+            {navigationType()}
+
             <Drawer
                 anchor='left'
                 open={open}
@@ -33,7 +53,12 @@ const TitleBar = styled(AppBar)({
     backgroundColor: 'grey',
 });
 
-const MenuButton = styled(MenuIcon)({
+const MenuButton = styled(Menu)({
+    color: 'white',
+    fontSize: '50px'
+});
+
+const BackButton = styled(ArrowBack)({
     color: 'white',
     fontSize: '50px'
 });
