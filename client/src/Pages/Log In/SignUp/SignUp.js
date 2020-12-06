2e96a0cf9bc6f6a59fd5a134eadcb276b41a8d06
Jedro1270 <jedro1270@gmail.com>
2020-12-06 09:48:17 +0800

    feat: add sign up logic

--- client/src/Pages/Log In/SignUp/SignUp.js
+++ client/src/Pages/Log In/SignUp/SignUp.js
@@ -1,11 +1,28 @@
 import { Grid, Link, TextField, styled, Box } from '@material-ui/core';
+import Axios from 'axios';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Title from '../Title';
 import AppLogo from '../AppLogo';
 import SubmitButton from '../SubmitButton';
 
 export default function SignUp() {
+
+  const [registerUsername, setRegisterUsername] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
+
+  const register = () => {
+    Axios({
+      method: 'POST',
+      data: {
+        username: registerUsername,
+        password: registerPassword
+      },
+       withCredentials: true,
+       url: 'http://localhost:2727/sign-up'
+    }).then((response) => console.log(response))
+  }
+
   return (
     <SignUpBody>
 
@@ -26,6 +43,7 @@ export default function SignUp() {
                 id='username'
                 label='Username'
                 autoFocus
+                onChange={(event) => setRegisterUsername(event.target.value)}
               />
             </Grid>
 
@@ -39,25 +57,13 @@ export default function SignUp() {
                 type='password'
                 id='password'
                 autoComplete='current-password'
-              />
-            </Grid>
-
-            <Grid item xs={12}>
-              <TextField
-                variant='outlined'
-                required
-                fullWidth
-                name='confirmPassword'
-                label='Confirm Password'
-                type='password'
-                id='confirmPassword'
-                autoComplete='current-password'
+                onChange={(event) => setRegisterPassword(event.target.value)}
               />
             </Grid>
 
           </Grid>
 
-          <SubmitButton text='SUBMIT'/>
+          <SubmitButton text='SUBMIT' submit={register}/>
 
           <Grid container justify='flex-end'>
             <Grid item>
@@ -73,7 +79,7 @@ export default function SignUp() {
 }
 
 const SignUpBody = styled(Box)({
-  padding: '20px',
+  padding: '5px',
   component: 'main',
   maxWidth: 'false',
   width: '100%',
