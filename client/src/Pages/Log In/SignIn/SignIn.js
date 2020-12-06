a266348360dcc023e3ae324c5665e742aa1ceab7
Jedro1270 <jedro1270@gmail.com>
2020-12-06 09:48:00 +0800

    feat: add sign in logic

--- client/src/Pages/Log In/SignIn/SignIn.js
+++ client/src/Pages/Log In/SignIn/SignIn.js
@@ -1,11 +1,28 @@
 import { Grid, Link, TextField, styled, Box } from '@material-ui/core';
+import Axios from 'axios';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Title from '../Title';
 import AppLogo from '../AppLogo';
 import SubmitButton from '../SubmitButton';
 
-export default function SignUp() {
+export default function SignIn() {
+
+  const [loginUsername, setLoginUsername] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+
+  const logIn = () => {
+    Axios({
+      method: 'POST',
+      data: {
+        username: loginUsername,
+        password: loginPassword
+      },
+       withCredentials: true,
+       url: 'http://localhost:2727/sign-in'
+    }).then((response) => console.log(response))
+  }
+
   return (
     <SignInBody>
 
@@ -26,6 +43,7 @@ export default function SignUp() {
                 id='username'
                 label='Username'
                 autoFocus
+                onChange={(event) => setLoginUsername(event.target.value)}
               />
             </Grid>
 
@@ -39,12 +57,13 @@ export default function SignUp() {
                 type='password'
                 id='password'
                 autoComplete='current-password'
+                onChange={(event) => setLoginPassword(event.target.value)}
               />
             </Grid>
 
           </Grid>
 
-          <SubmitButton text='SIGN IN'/>
+          <SubmitButton text='SIGN IN' submit={logIn}/>
 
           <Grid container justify='flex-end'>
             <Grid item>
