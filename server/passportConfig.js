6c80d9650068ffadffcddd0a6d59a01c50eb77d3
Jedro1270 <jedro1270@gmail.com>
2020-12-06 09:50:12 +0800

    feat: create passport configuration helper function

---
+++ server/passportConfig.js
@@ -0,0 +1,56 @@
+import bcrypt from 'bcryptjs';
+import { Strategy as localStrategy } from 'passport-local';
+
+export default function passportStrategy(passport, database) {
+
+    passport.use(
+        new localStrategy((username, password, done) => {
+            database.query(
+                `
+                    SELECT * FROM "users"
+                        WHERE "username" = '${username}';
+                `,
+                (error, results) => {
+                    if (error) {
+                        console.log(`ERROR: ${error}`)
+                      } else {
+
+                        if (results.rows.length === 0) {
+                          return done(null, false)
+                        }
+
+                        const user = results.rows[0];
+
+                        bcrypt.compare( password, user.password, (error, result) => {
+                            if (error) {
+                                console.log(`ERROR: ${error}`)
+                            } else if (result === true) {
+                                return done(null, user);
+                            } else {
+                                return done(null, false);
+                            }
+                        });
+                      }
+                }
+            )
+        })
+    );
+
+    passport.serializeUser((user, done) => {
+        done(null, user.user_id)
+    });
+
+    passport.deserializeUser((id, done) => {
+        database.query(
+            `
+                SELECT * FROM 'users'
+                    WHERE "user_id" = ${id}
+            `,
+            (error, results) => {
+                const user = results.rows[0];
+
+                done(error, user)
+            }
+        )
+    })
+}
