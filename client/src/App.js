1a4070f389a59f1438882ff74b47a3502d79a62e
Jedro1270 <jedro1270@gmail.com>
2020-12-10 14:19:12 +0800

    refactor: remove unecessary props due to redux conversion

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import {Route, BrowserRouter as Router, Switch, Redirect} from 'react-router-dom';
 
 import Header from './Pages/Main App/Header/Header';
-import Groups from './Pages/Main App/Body/Groups/Groups';
+import Groups from './Pages/Main App/Body/Groups/Groups Page/Groups';
 import Pomodoro from './Pages/Main App/Body/Pomodoro/Pomodoro';
 import Notebooks from './Pages/Main App/Body/Notebooks/Notebooks';
 import Chat from './Pages/Main App/Body/Chat/Chat';
@@ -16,9 +16,6 @@ import JoinGroup from './Pages/Main App/Body/Groups/Join Group/JoinGroup';
 import CreateGroup from './Pages/Main App/Body/Groups/Create Group/CreateGroup';
 
 export default function App() {
-  
-  const [title, setTitle] = useState('');
-  const [backButtonNav, setBackButtonNav] = useState(false);
 
   return (
     <Router>
@@ -37,72 +34,72 @@ export default function App() {
             />
             <Route exact path='/groups' render={() => (
               <div className='App-body'>
-                  <Header title={title} backButtonNav={backButtonNav}/>
-                  <Groups setTitle={setTitle} setBackButtonNav={setBackButtonNav}/>
+                  <Header/>
+                  <Groups/>
               </div>
                 
               )} 
             />
             <Route exact path='/groups/join-group' render={() => (
               <div className='App-body'>
-                  <Header title={title} backButtonNav={backButtonNav}/>
-                  <JoinGroup setTitle={setTitle} setBackButtonNav={setBackButtonNav}/>
+                  <Header/>
+                  <JoinGroup/>
               </div>
 
               )} 
             />
             <Route exact path='/groups/create-group' render={() => (
               <div className='App-body'>
-                  <Header title={title} backButtonNav={backButtonNav}/>
-                  <CreateGroup setTitle={setTitle} setBackButtonNav={setBackButtonNav}/>
+                  <Header/>
+                  <CreateGroup />
               </div>
 
               )} 
             />
             <Route exact path='/pomodoro' render={() => (
                 <div className='App-body'>
-                  <Header title={title} backButtonNav={backButtonNav}/>
-                  <Pomodoro setTitle={setTitle}/>
+                  <Header/>
+                  <Pomodoro />
                 </div>
                 
               )} 
             />
             <Route exact path='/notebooks' render={() => (
                 <div className='App-body'>
-                  <Header title={title} backButtonNav={backButtonNav}/>
-                  <Notebooks setTitle={setTitle} setBackButtonNav={setBackButtonNav}/>
+                  <Header/>
+                  <Notebooks />
                 </div>
                 
               )} 
             />
             <Route exact path='/notebooks/:notebookTitle' render={() => (
                 <div className='App-body'>
-                  <Header title={title} backButtonNav={backButtonNav}/>
-                  <Notes setTitle={setTitle} setBackButtonNav={setBackButtonNav}/>
+                  <Header/>
+                  <Notes />
                 </div>
                 
               )} 
             />
             <Route exact path='/notebooks/:notebookTitle/:noteTitle' render={() => (
                 <div className='App-body'>
-                  <Header title={title} backButtonNav={backButtonNav}/>
-                  <NoteContents setTitle={setTitle} setBackButtonNav={setBackButtonNav}/>
+                  <Header/>
+                  <NoteContents />
                 </div>
                 
               )} 
             />
             <Route exact path='/chat' render={() => (
                 <div className='App-body'>
-                  <Header title={title} backButtonNav={backButtonNav}/>
-                  <Chat setTitle={setTitle}/>
+                  <Header/>
+                  <Chat />
                 </div>
                 
               )} 
             />
             <Route exact path='/rankings' render={() => (
                 <div className='App-body'>
-                  <Header title={title} backButtonNav={backButtonNav}/>
-                  <Rankings setTitle={setTitle} title='Rankings and Badges'/>
+                  <Header/>
+                  <Rankings/>
                 </div>
                 
               )} 
