878435254596aa120f21da52c79b74b81ef693c1
Jedro1270 <jedro1270@gmail.com>
2020-12-10 14:22:36 +0800

    feat: create change header actions

---
+++ client/src/Redux/Actions/ChangeHeaderNavigation.js
@@ -0,0 +1,13 @@
+const setBackButton = () => {
+    return {
+        type: 'USE_BACK_BUTTON'
+    }
+}
+
+const setDrawer = () => {
+    return {
+        type: 'USE_DRAWER'
+    }
+}
+
+export { setBackButton, setDrawer };
