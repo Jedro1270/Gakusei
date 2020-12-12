e0585201ee40619da6ff37ae5be25a2601503a50
Jedro1270 <jedro1270@gmail.com>
2020-12-12 11:10:55 +0800

    feat: add user state in combined reducer

--- client/src/Redux/Reducers/index.js
+++ client/src/Redux/Reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import headerTitleReducer from './HeaderTitle';
 import headerNavigationReducer from './HeaderNavigation';
+import userStateReducer from './UserState';
 
 const allReducers = combineReducers({
     headerTitle: headerTitleReducer,
-    headerNavigation: headerNavigationReducer
+    headerNavigation: headerNavigationReducer,
+    userState: userStateReducer
 });
 
 export default allReducers;
