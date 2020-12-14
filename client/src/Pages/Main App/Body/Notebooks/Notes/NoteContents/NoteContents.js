af50913046d96b7459b675984651e3fa3f2c0b23
Jedro1270 <jedro1270@gmail.com>
2020-12-14 15:04:59 +0800

    feat: add save notes feature for note contents

--- client/src/Pages/Main App/Body/Notebooks/Notes/NoteContents/NoteContents.js
+++ client/src/Pages/Main App/Body/Notebooks/Notes/NoteContents/NoteContents.js
@@ -1,16 +1,25 @@
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { Alert } from '@material-ui/lab';
+import { Box, Button, styled, Typography } from "@material-ui/core";
 
 import { setBackButton } from "../../../../../../Redux/Actions/ChangeHeaderNavigation";
 import changeTitle from "../../../../../../Redux/Actions/ChangeTitle";
+import CustomAjax from '../../../../../../CustomAjax';
 
 export default function NoteContents() {
 
     const { noteTitle } = useParams();
+    const [contents, setContents] = useState('');
+    const [noteSaved, setNoteSaved] = useState(false);
 
     const dispatch = useDispatch();
+    const location = useLocation();
+    const noteID = location.state.noteID;
 
     const createTitle = (noteURL) => {
         return noteURL
@@ -22,14 +31,93 @@ export default function NoteContents() {
                         });
     }
 
+    const updateNoteContents = (noteContents) => {
+        const data = {
+            contents: noteContents,
+            noteID: noteID,
+        }
+
+        const ajax = new CustomAjax();
+
+        ajax.put('http://localhost:2727/notebooks/notes/note-contents', data, true);
+        ajax.stateListener((response) => {
+            response = JSON.parse(response);
+
+            setContents(response.note.note_content);
+        });
+    }
+
+    const getNoteContents = () => {
+        const ajax = new CustomAjax();
+
+        ajax.get('http://localhost:2727/notebooks/notes');
+        ajax.stateListener((response) => {
+            response = JSON.parse(response);
+
+            const currentNote = response.notes.filter((note) => {
+                if (note.note_id === noteID) {
+                    return true;
+                }
+
+                return false;
+            })[0];
+
+            const currentNoteContents = currentNote.note_content
+
+            if (contents.length < currentNoteContents.length) {
+                setContents(currentNoteContents);
+            }
+        });
+    }
+
+    useEffect(() => {
+        getNoteContents();
+    }, []);
+
     dispatch(changeTitle(createTitle(noteTitle)));
     dispatch(setBackButton());
 
+    let noteSavedAlert = null;
+
+    if (noteSaved) {
+        noteSavedAlert = <Alert severity='success' onClose={() => {setNoteSaved(false)}}>Note Saved!</Alert>
+    }
+
     return (
-       <div>
+       <NoteContentsPage>
+           {noteSavedAlert}
            <CKEditor
                 editor={ClassicEditor}
+                data={contents}
+                onChange={(event, editor) => {setContents(editor.getData())}}
            />
-       </div>
+           <SaveButton>
+               <SaveButtonText 
+                    onClick={() => {
+                        updateNoteContents(contents);
+                        setNoteSaved(true);
+                    }}
+                >
+                    SAVE
+               </SaveButtonText>
+           </SaveButton>
+       </NoteContentsPage>
     );
 }
+
+const SaveButton = styled(Button)({
+    backgroundColor: 'white',
+    '&:hover': {
+        backgroundColor: 'lightgrey'
+    },
+    margin: '20px',
+});
+
+const SaveButtonText = styled(Typography)({
+    fontWeight: 'bold',
+    fontSize: '25px'
+});
+
+const NoteContentsPage = styled(Box)({
+    backgroundColor: 'black',
+});
