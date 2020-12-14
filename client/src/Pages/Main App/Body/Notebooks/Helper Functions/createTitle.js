95ef19e020f72d69d242e66b6a3c1fdc4595e009
Jedro1270 <jedro1270@gmail.com>
2020-12-14 15:14:41 +0800

    feat: create a function to createTitle

---
+++ client/src/Pages/Main App/Body/Notebooks/Helper Functions/createTitle.js
@@ -0,0 +1,11 @@
+const createTitle = (name) => {
+    return name
+            .replaceAll('-', ' ')
+            .split(' ')
+            .map((word) => {
+                const firstLetter = word[0].toUpperCase();
+                return `${firstLetter}${word.slice(1)} `
+            });
+}
+
+export default createTitle;
