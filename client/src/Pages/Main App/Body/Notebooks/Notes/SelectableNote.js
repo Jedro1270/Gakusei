695d99fa9cb12b0b54db069c9d6fcf8ed8f5a5f9
Jedro1270 <jedro1270@gmail.com>
2020-12-14 08:07:57 +0800

    feat: create selectable note component

---
+++ client/src/Pages/Main App/Body/Notebooks/Notes/SelectableNote.js
@@ -0,0 +1,55 @@
+import { ListItem, Typography, styled } from "@material-ui/core";
+import { Link } from "react-router-dom";
+
+export default function Notes(props) {
+
+    return (
+
+            <CustomLink to={{pathname: `/notebooks/${props.notebookTitle}/${props.noteURL}`, state: {noteID: props.noteID}}}>
+                <Note button>
+                    <DateText>
+                        {props.dateEdited}
+                    </DateText>
+
+                    <NoteTitle>
+                        {props.noteTitle}
+                    </NoteTitle>
+
+                    <NotePreview noWrap>
+                        {props.contents}
+                    </NotePreview>
+                </Note>
+            </CustomLink>
+    );
+}
+
+const CustomLink = styled(Link)({
+    textDecoration: 'none',
+    color: 'white'
+});
+
+const Note = styled(ListItem)({
+    backgroundColor: 'grey',
+    padding: '15px',
+    margin: '10px 0px',
+    '&:hover': {
+        backgroundColor: 'rgb(102, 102, 102)'
+    },
+    display: 'block',
+    textAlign: 'left',
+    color: 'white'
+});
+
+const NoteTitle = styled(Typography)({
+    fontSize: '30px',
+    fontWeight: 'bold'
+});
+
+const DateText = styled(Typography)({
+    fontSize: '15px'
+});
+
+const NotePreview = styled(Typography)({
+    fontSize: '20px',
+    width: '80%'
+});
