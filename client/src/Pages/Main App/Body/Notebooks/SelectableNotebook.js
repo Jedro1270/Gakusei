6520b05d1a4785484da706d08850aa5a1c81688b
Jedro1270 <jedro1270@gmail.com>
2020-12-14 08:04:23 +0800

    feat: add link state

--- client/src/Pages/Main App/Body/Notebooks/SelectableNotebook.js
+++ client/src/Pages/Main App/Body/Notebooks/SelectableNotebook.js
@@ -6,7 +6,7 @@ export default function SelectableNotebook(props) {
 
     return (
 
-        <CustomLink to={`/notebooks/${props.notebookURL}`}>
+        <CustomLink to={{ pathname: `/notebooks/${props.notebookURL}`, state: {notebookID: props.notebookID}}}>
             <Notebook button>
                 <NotebookTitle>
                     {props.title}
