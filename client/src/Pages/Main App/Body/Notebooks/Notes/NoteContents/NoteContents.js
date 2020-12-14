e65e61d7c57779c8d822a96142f0f085fc5e5831
Jedro1270 <jedro1270@gmail.com>
2020-12-14 08:08:38 +0800

    feat: create note contents main body with editor

--- client/src/Pages/Main App/Body/Notebooks/Notes/NoteContents/NoteContents.js
+++ client/src/Pages/Main App/Body/Notebooks/Notes/NoteContents/NoteContents.js
@@ -1,20 +1,35 @@
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
 import { setBackButton } from "../../../../../../Redux/Actions/ChangeHeaderNavigation";
 import changeTitle from "../../../../../../Redux/Actions/ChangeTitle";
 
 export default function NoteContents() {
 
-    // const { noteTitle } = useParams();
+    const { noteTitle } = useParams();
 
     const dispatch = useDispatch();
 
-    dispatch(changeTitle(''));
+    const createTitle = (noteURL) => {
+        return noteURL
+                        .replaceAll('-', ' ')
+                        .split(' ')
+                        .map((word) => {
+                            const firstLetter = word[0].toUpperCase();
+                            return `${firstLetter}${word.slice(1)} `
+                        });
+    }
+
+    dispatch(changeTitle(createTitle(noteTitle)));
     dispatch(setBackButton());
 
     return (
        <div>
-
+           <CKEditor
+                editor={ClassicEditor}
+           />
        </div>
     );
 }
