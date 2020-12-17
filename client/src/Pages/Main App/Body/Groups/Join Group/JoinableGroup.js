fd26253b7aed785746b3d666506a1d0fd0cf47d8
Jedro1270 <jedro1270@gmail.com>
2020-12-17 11:40:16 +0800

    feat: create join group request

--- client/src/Pages/Main App/Body/Groups/Join Group/JoinableGroup.js
+++ client/src/Pages/Main App/Body/Groups/Join Group/JoinableGroup.js
@@ -1,9 +1,35 @@
 import React, { useState } from 'react';
 import { Avatar, Typography, styled, Button, Dialog, List, ListItem } from '@material-ui/core';
+import CustomAjax from '../../../../../CustomAjax';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 export default function JoinableGroup(props) {
 
     const [openDialog, setOpenDialog] = useState(false);
+
+    const token = useSelector((state) => { return state.tokenState });
+    const user = useSelector((state) => { return state.userState });
+    const history = useHistory();
+
+    const joinGroup = () => {
+        const data = {
+            groupId: props.groupId,
+            userId: user.id
+        }
+
+        const ajax = new CustomAjax();
+
+        ajax.post('http://localhost:2727/api/groups/join-group', data, true, token);
+        ajax.stateListener((response) => {
+            response = JSON.parse(response);
+            if (response.message === 'Join Group Successful') {
+                history.push('/api/groups');
+            } else if (response.message === 'Group Already Joined') {
+                alert('group joined already')
+            }
+        });
+    }
 
     return (
         <div>
@@ -17,7 +43,7 @@ export default function JoinableGroup(props) {
 
             <Dialog open={openDialog} onClose={() => { setOpenDialog(false) }}>
                 <List>
-                    <ListItem button>
+                    <ListItem button onClick={() => {joinGroup()}}>
                         <SelectGroupText>
                             Join Group
                         </SelectGroupText>
