be0743547e1d8041a6b422d32aaf058abd8970b6
Jedro1270 <jedro1270@gmail.com>
2020-12-17 09:18:18 +0800

    feat: show groups joined by the user only

--- server/Routes/groups.js
+++ server/Routes/groups.js
@@ -1,10 +1,13 @@
 export default function groupsRoutes(app, secureRoute, upload, database) {
   app.get('/api/groups', secureRoute, (request, response) => {
     try {
-      database.query( // Select all from user joined groups only
+      database.query(
         `
-              SELECT * FROM "groups";
-            `,
+          SELECT * FROM "groups"
+            INNER JOIN "group_memberships" as gm
+              USING (group_id)
+          WHERE gm.user_id = $1
+        `, [request.user.id],
         (error, results) => {
           if (error) {
             console.log(`ERROR: ${error}`)
@@ -12,13 +15,13 @@ export default function groupsRoutes(app, secureRoute, upload, database) {
             response.json({ groups: results.rows });
           }
         }
-      )
+      );
     } catch (error) {
       console.log(error);
     }
   });
 
-  app.post('/api/groups/join-group/search', (request, response) => {
+  app.post('/api/groups/join-group/search', secureRoute, (request, response) => {
     try {
       database.query(
         `
@@ -38,13 +41,13 @@ export default function groupsRoutes(app, secureRoute, upload, database) {
     }
   });
 
-  app.post('/api/groups/create-group', upload.single('file'), (request, response) => {
+  app.post('/api/groups/create-group', secureRoute, upload.single('file'), (request, response) => {
     try {
       database.query(
         `
           INSERT INTO "groups"(group_name, group_picture)
             VALUES($1, $2)
-            RETURNING *;
+          RETURNING *;
         `, [request.body.groupname, request.file.filename],
         (error, results) => {
           if (error) {
@@ -53,8 +56,35 @@ export default function groupsRoutes(app, secureRoute, upload, database) {
             if (results.rows.length === 0) {
               console.log('ERROR: Data not inserted to database!');
             } else {
-              console.log('Group inserted');
-              response.json({ message: 'Group Inserted' });
+              try {
+                database.query(
+                  `
+                    INSERT INTO "group_memberships"(user_id, group_id)
+                      VALUES(
+                        $1, 
+                        (SELECT "group_id" FROM "groups"
+                          WHERE "group_name" = $2
+                        )
+                      )
+                    RETURNING *;
+                  `, [request.user.id, request.body.groupname],
+                  (error, results) => {
+                    if (error) {
+                      console.log(`ERROR: ${error}`)
+                    } else {
+                      if (results.rows.length === 0) {
+                        console.log('ERROR: Data not inserted to database!');
+                      } else {
+
+                        console.log('Group inserted');
+                        response.json({ message: 'Group Inserted' });
+                      }
+                    }
+                  }
+                );
+              } catch (error) {
+                console.log(error)
+              }
             }
           }
         }
