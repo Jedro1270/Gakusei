352fddc2c00f508e31918991dacdae2b309c345a
Jedro1270 <jedro1270@gmail.com>
2020-12-17 09:14:55 +0800

    feat: add user state reducer to combined reducers

--- client/src/Redux/Reducers/index.js
+++ client/src/Redux/Reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import headerTitleReducer from './HeaderTitle';
 import headerNavigationReducer from './HeaderNavigation';
 import tokenStateReducer from './TokenState';
+import userStateReducer from './UserState';
 
 const allReducers = combineReducers({
     headerTitle: headerTitleReducer,
     headerNavigation: headerNavigationReducer,
-    tokenState: tokenStateReducer
+    tokenState: tokenStateReducer,
+    userState: userStateReducer
 });
 
 export default allReducers;
