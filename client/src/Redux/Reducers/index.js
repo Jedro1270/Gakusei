5ec895aced9881ed1a9077ed837c99877c519fdb
Jedro1270 <jedro1270@gmail.com>
2020-12-17 15:32:51 +0800

    feat: add current group state reducer to combined reducer

--- client/src/Redux/Reducers/index.js
+++ client/src/Redux/Reducers/index.js
@@ -4,12 +4,14 @@ import headerTitleReducer from './HeaderTitle';
 import headerNavigationReducer from './HeaderNavigation';
 import tokenStateReducer from './TokenState';
 import userStateReducer from './UserState';
+import currentGroupStateReducer from './CurrentGroupState';
 
 const allReducers = combineReducers({
     headerTitle: headerTitleReducer,
     headerNavigation: headerNavigationReducer,
     tokenState: tokenStateReducer,
-    userState: userStateReducer
+    userState: userStateReducer,
+    currentGroupState: currentGroupStateReducer
 });
 
 export default allReducers;
