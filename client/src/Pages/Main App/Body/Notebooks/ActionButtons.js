230608c48ff4d42176ae2f7b4e9035c367371fe7
Jedro1270 <jedro1270@gmail.com>
2020-12-28 14:52:28 +0800

    feat: create action buttons component

---
+++ client/src/Pages/Main App/Body/Notebooks/ActionButtons.js
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { Typography, Button, styled, Box } from '@material-ui/core';
+
+import DeleteDialog from './Dialogs/DeleteDialog';
+import RenameDialog from './Dialogs/RenameDialog';
+
+export default function ActionButtons(props) {
+
+    const [openRenameDialog, setOpenRenameDialog] = useState(false);
+    const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+
+    return (
+        <ActionButtonsBody>
+
+            <ActionButton onClick={() => { setOpenRenameDialog(true) }}>
+                <ActionButtonText>
+                    Rename
+                </ActionButtonText>
+            </ActionButton>
+
+            <ActionButton onClick={() => { setOpenDeleteDialog(true) }}>
+                <DeleteButtonText>
+                    Delete
+                </DeleteButtonText>
+            </ActionButton>
+
+            <DeleteDialog
+                setOpenDialog={ setOpenDeleteDialog }
+                openDialog={ openDeleteDialog }
+                delete={ props.delete }
+                label={ props.label }
+                selectedTitle={ props.selectedTitle }
+            />
+
+            <RenameDialog
+                setOpenDialog={ setOpenRenameDialog }
+                openDialog={ openRenameDialog }
+                rename={ props.rename }
+                setNewTitle={ props.setNewTitle }
+                label={ props.label }
+                selectedTitle={ props.selectedTitle }
+            />
+
+        </ActionButtonsBody>
+    );
+}
+
+const ActionButtonsBody = styled(Box)({
+    marginLeft: 'auto',
+    backgroundColor: 'grey',
+    padding: '15px',
+    margin: '10px 0px',
+    display: 'flex',
+    flex: '1'
+});
+
+const ActionButton = styled(Button)({
+    margin: '5px',
+    backgroundColor: 'lightgrey',
+    '&:hover': {
+        backgroundColor: 'darkgrey'
+    },
+    flex: '1'
+});
+
+const ActionButtonText = styled(Typography)({
+    fontWeight: 'bold',
+    fontSize: '100%'
+});
+
+const DeleteButtonText = styled(ActionButtonText)({
+    color: 'red'
+});
