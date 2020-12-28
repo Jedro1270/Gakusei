f92ebdb01a4dc2117ebe8c7d8a309e51efa244c3
Jedro1270 <jedro1270@gmail.com>
2020-12-28 15:02:55 +0800

    feat: create update and delete routes for notes and notebooks

--- server/Routes/notebooks.js
+++ server/Routes/notebooks.js
@@ -49,6 +49,70 @@ export default function notebooksRoutes(app, secureRoute, database) {
         }
     });
 
+    // Update Notebook Title
+    app.put('/api/notebooks/:groupId/:notebookId', secureRoute, (request, response) => {
+        const notebookId = request.params.notebookId;
+        const title = request.body.title;
+
+        try {
+            database.query(
+                `
+                UPDATE "notebooks" 
+                SET 
+                    "notebook_name" = $1
+                WHERE "notebook_id" = $2
+                RETURNING *;
+                `, [title, notebookId],
+                (error, results) => {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        response.json({ note: results.rows[0] });
+                    }
+                }
+            )
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    // Delete Notebook
+    app.delete('/api/notebooks/:groupId/:notebookId', secureRoute, (request, response) => {
+        const notebookId = request.params.notebookId;
+
+        try {
+            database.query(
+                `
+                DELETE FROM "notes"
+                    WHERE "notebook_id" = $1
+                RETURNING *;
+                `, [notebookId],
+                (error, results) => {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        database.query(
+                            `
+                            DELETE FROM "notebooks"
+                                WHERE "notebook_id" = $1
+                            RETURNING *;
+                            `, [notebookId],
+                            (error, results) => {
+                                if (error) {
+                                    console.log(error)
+                                } else {
+                                    response.json({ note: results.rows[0] });
+                                }
+                            }
+                        );
+                    }
+                }
+            );
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
     // Get All Notes of Notebook
     app.get('/api/notebooks/:groupId/:notebookId', secureRoute, (request, response) => {
         const notebookId = request.params.notebookId;
@@ -107,6 +171,57 @@ export default function notebooksRoutes(app, secureRoute, database) {
                 `
                 SELECT * FROM "notes"
                     WHERE "note_id" = $1;
+                `, [noteId],
+                (error, results) => {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        response.json({ note: results.rows[0] });
+                    }
+                }
+            )
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    // Update Note Title
+    app.put('/api/notebooks/:groupId/:notebookId/:noteId', secureRoute, (request, response) => {
+        const noteId = request.params.noteId;
+        const title = request.body.title;
+
+        try {
+            database.query(
+                `
+                UPDATE "notes" 
+                SET 
+                    "note_title" = $1
+                WHERE "note_id" = $2
+                RETURNING *;
+                `, [title, noteId],
+                (error, results) => {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        response.json({ note: results.rows[0] });
+                    }
+                }
+            )
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+     // Delete Note
+     app.delete('/api/notebooks/:groupId/:notebookId/:noteId', secureRoute, (request, response) => {
+        const noteId = request.params.noteId;
+
+        try {
+            database.query(
+                `
+                DELETE FROM "notes" 
+                    WHERE "note_id" = $1
+                RETURNING *;
                 `, [noteId],
                 (error, results) => {
                     if (error) {
