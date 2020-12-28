1eeeaf955b7698d5446c5d788e47d7aaa5427fc0
Jedro1270 <jedro1270@gmail.com>
2020-12-28 14:56:30 +0800

    refactor: factor out create dialog component

--- client/src/Pages/Main App/Body/Notebooks/FloatingActionButton.js
+++ client/src/Pages/Main App/Body/Notebooks/FloatingActionButton.js
@@ -1,6 +1,8 @@
-import { Fab, styled, Typography, TextField, Dialog, DialogTitle, DialogActions, Button } from '@material-ui/core';
+import { Fab, styled } from '@material-ui/core';
 import { useState } from 'react';
 import { Add } from '@material-ui/icons';
+
+import CreateDialog from './Dialogs/CreateDialog';
 
 export default function FloatingActionButton(props) {
 
@@ -12,30 +14,13 @@ export default function FloatingActionButton(props) {
                 <Add />
             </ActionButton>
 
-            <Dialog open={openDialog} onClose={() => { setOpenDialog(false) }}>
-                <DialogTitle>
-                    <FormTitle>
-                        Create {props.label}
-                    </FormTitle>
-                </DialogTitle>
-
-                <NoteBookNameInput
-                    autoFocus
-                    label={`${props.label} Name`}
-                    onChange={(event) => { props.setNewName(event.target.value) }}
-                />
-
-                <DialogActions>
-                    <Button onClick={() => {
-                        props.handleDialogButtonClick();
-                        setOpenDialog(false);
-                    }}>
-                        <CreateButtonText>
-                            Create
-                        </CreateButtonText>
-                    </Button>
-                </DialogActions>
-            </Dialog>
+            <CreateDialog 
+                openDialog={ openDialog }
+                setOpenDialog={ setOpenDialog }
+                label={ props.label }
+                setNewName={ props.setNewName }
+                create={ props.handleDialogButtonClick }
+            />
         </div>
     )
 }
@@ -44,17 +29,4 @@ const ActionButton = styled(Fab)({
     position: 'fixed',
     right: '20px',
     bottom: '20px'
-});
-
-const FormTitle = styled(Typography)({
-    fontSize: '30px',
-    fontWeight: 'bold'
-});
-
-const NoteBookNameInput = styled(TextField)({
-    margin: '0px 20px 20px 20px',
-});
-
-const CreateButtonText = styled(Typography)({
-    fontWeight: 'bold'
 });
