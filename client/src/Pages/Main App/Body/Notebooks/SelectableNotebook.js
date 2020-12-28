f0103277ac8951341ce4feb6f02e5610c75c2a20
Jedro1270 <jedro1270@gmail.com>
2020-12-28 14:58:06 +0800

    feat: add rename and delete buttons and functionalities

--- client/src/Pages/Main App/Body/Notebooks/SelectableNotebook.js
+++ client/src/Pages/Main App/Body/Notebooks/SelectableNotebook.js
@@ -1,30 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { ListItem, Typography, styled } from '@material-ui/core';
+import { ListItem, Typography, styled, Box } from '@material-ui/core';
+import ActionButtons from './ActionButtons';
+import CustomAjax from '../../../../CustomAjax';
+import { useSelector } from 'react-redux';
 
 export default function SelectableNotebook(props) {
 
+    const [newTitle, setNewTitle] = useState('');
+
+    const currentGroup = useSelector((state) => { return state.currentGroupState });
+    const token = useSelector((state) => { return state.tokenState });
+    const ajax = new CustomAjax();
+
+    const renameNotebook = () => {
+        const data = {
+            title: newTitle
+        }
+
+        ajax.put(`http://localhost:2727/api/notebooks/${currentGroup.id}/${props.notebookID}`, data, true, token);
+        ajax.stateListener((response) => {
+            response = JSON.parse(response);
+
+            props.loadNotebooks(true);
+        });
+    }
+
+    const deleteNotebook = () => {
+        ajax.delete(`http://localhost:2727/api/notebooks/${currentGroup.id}/${props.notebookID}`, token);
+        ajax.stateListener((response) => {
+            response = JSON.parse(response);
+
+            props.loadNotebooks(true);
+        });
+    }
+
     return (
 
-        <CustomLink to={{ pathname: `/api/notebooks/${props.notebookURL}`, state: { notebookID: props.notebookID } }}>
-            <Notebook button>
-                <NotebookTitle>
-                    {props.title}
-                </NotebookTitle>
-            </Notebook>
-        </CustomLink>
+        <SelectableNotebookBody>
+            <CustomLink to={{ pathname: `/api/notebooks/${props.notebookURL}`, state: { notebookID: props.notebookID } }}>
+                <Notebook button>
+                    <NotebookTitle>
+                        {props.title}
+                    </NotebookTitle>
+                </Notebook>
+            </CustomLink>
+
+            <ActionButtons 
+                setNewTitle = { setNewTitle }
+                rename={ renameNotebook }
+                delete={ deleteNotebook }
+                label={ 'Notebook' }
+                selectedTitle={ props.title }
+            />
+        </SelectableNotebookBody>
 
     );
 }
 
+const SelectableNotebookBody = styled(Box)({
+    display: 'flex',
+    flexDirection: 'row'
+});
+
 const CustomLink = styled(Link)({
     textDecoration: 'none',
-    color: 'white'
+    color: 'white',
+    flex: '3'
 });
 
 const Notebook = styled(ListItem)({
     backgroundColor: 'grey',
-    padding: '10px',
+    padding: '15px',
     margin: '10px 0px',
     '&:hover': {
         backgroundColor: 'rgb(102, 102, 102)'
