b69eae559e4e0512a1d45f3162a1020740aad57f
Jedro1270 <jedro1270@gmail.com>
2021-01-01 17:43:18 +0800

    feat: create no group selected dialog component file

---
+++ client/src/Pages/Main App/Body/Error Dialogs/NoGroupSelectedDialog.js
@@ -0,0 +1,54 @@
+import { Box, styled, Typography, Button, TextField, Dialog, DialogContent, DialogActions } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+
+export default function NoGroupSelectedDialog(props) {
+
+    const [openDialog, setOpenDialog] = useState(false);
+
+    const history = useHistory();
+
+    const currentGroup = useSelector((state) => { return state.currentGroupState });
+
+    useEffect(() => {
+        if (currentGroup.id == null) {
+            setOpenDialog(true);
+        }
+    }, []);
+
+    return (
+            <Dialog open={openDialog}>
+                <BoldDialogTitle>
+                    No Group Selected
+                </BoldDialogTitle>
+
+                <DialogContent>
+                    Please select a group fom the Groups Page before accessing {props.page}.
+                </DialogContent>
+
+                <DialogActions>
+                    <Button onClick={() => { 
+                        setOpenDialog(false);
+                        history.push('/api/groups');
+                    }}>
+                        <BoldDialogAction>
+                            Close
+                        </BoldDialogAction>
+                    </Button>
+                </DialogActions>
+            </Dialog>
+    );
+}
+
+const BoldDialogAction = styled(Typography)({
+    fontWeight: 'bold'
+});
+
+const BoldDialogTitle = styled(Typography)({
+    fontSize: '30px',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: '20px'
+});
