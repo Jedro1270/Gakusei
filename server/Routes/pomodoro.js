742d58d12ee64cf1f1135f5cf30c18c6f4290ec2
Jedro1270 <jedro1270@gmail.com>
2021-01-01 15:37:52 +0800

    feat: add database queries pomodoro page routes

--- server/Routes/pomodoro.js
+++ server/Routes/pomodoro.js
@@ -1,7 +1,47 @@
 export default function pomodoroRoutes(app, secureRoute, database) {
-    app.get('/api/pomodoro', secureRoute, (request, response) => {
-        try {
 
+    // Get Pomodoros Completed of User
+    app.get('/api/pomodoro', secureRoute, (request, response) => {
+        const userId = request.user.id;
+
+        try {
+            database.query(
+                `
+                    SELECT * FROM "pomodoros"
+                    WHERE "user_id" = $1; 
+                `, [userId],
+                (error, results) => {
+                  if (error) {
+                    console.log(`ERROR: ${error}`);
+                  } else {
+                    response.json({ pomodoros: results.rows });
+                  }
+                }
+              );
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
+    // Record Completed Pomodoro
+    app.post('/api/pomodoro', secureRoute, (request, response) => {
+        const userId = request.user.id;
+
+        try {
+            database.query(
+                `
+                    INSERT INTO "pomodoros"(user_id)
+                        VALUES($1)
+                    RETURNING *;    
+                `, [userId],
+                (error, results) => {
+                  if (error) {
+                    console.log(`ERROR: ${error}`);
+                  } else {
+                    response.json({ message: 'Sucessfully Uploaded Pomodoro' });
+                  }
+                }
+              );
         } catch (error) {
             console.log(error);
         }
