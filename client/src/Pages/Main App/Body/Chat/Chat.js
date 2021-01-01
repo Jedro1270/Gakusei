9e154d635ae1cf59c615a878878b644f311abddf
Jedro1270 <jedro1270@gmail.com>
2021-01-01 10:37:45 +0800

    feat: create functional chat page body

--- client/src/Pages/Main App/Body/Chat/Chat.js
+++ client/src/Pages/Main App/Body/Chat/Chat.js
@@ -1,26 +1,199 @@
-import React, { useEffect } from 'react';
+import { Box, styled, Typography, Button, TextField, Icon, Dialog, DialogTitle, DialogContent, DialogActions } from '@material-ui/core';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { ChatFeed } from 'react-chat-ui';
+import SendIcon from '@material-ui/icons/Send';
 import { useHistory } from 'react-router-dom';
 
 import changeTitle from '../../../../Redux/Actions/ChangeTitle';
+import CustomAjax from '../../../../CustomAjax';
 import verifyToken from '../../Helper Functions/verifyToken';
 
-export default function Chat(props) {
+
+export default function Chat() {
+
+    const [messages, setMessages] = useState([]);
+    const [composedMessage, setComposedMessage] = useState('');
+    const [messagesLimit, setMessagesLimit] = useState(20);
+    const [openDialog, setOpenDialog] = useState(false);
 
     const token = useSelector((state) => { return state.tokenState });
-    const history = useHistory();
+    const currentGroup = useSelector((state) => { return state.currentGroupState });
+    const user = useSelector((state) => { return state.userState });
 
     const dispatch = useDispatch();
+    const history = useHistory();
 
-    useEffect(() => {
-        verifyToken(token, history);
-    }, [history, token]);
+    const ajax = new CustomAjax();
+
+    const getMessages = () => {
+        ajax.get(`http://localhost:2727/api/chat/${currentGroup.id}?limit=${messagesLimit}`, token);
+        ajax.stateListener((response) => {
+            response = JSON.parse(response);
+
+            const newMessages = [];
+
+            for (const message of response.messages) {
+                const parsedMessage = {
+                    id: message.author_id === user.id ? 0 : message.author_id,
+                    message: message.message_content,
+                    senderName: message.username
+                }
+
+                newMessages.unshift(parsedMessage);
+            }
+
+            setMessages(newMessages);
+        });
+    }
+    
+    const uploadMessages = (message) => {
+        const data = {
+            message: message
+        }
+
+        ajax.post(`http://localhost:2727/api/chat/${currentGroup.id}`, data, true, token);
+    }
+
+    const handleMessageSent = () => {
+        const message = {
+            id: 0,
+            message: composedMessage,
+        }
+
+        uploadMessages(message);
+    }
 
     dispatch(changeTitle('Chat'));
 
+    useEffect(() => {
+        verifyToken(token, history);
+
+        const interval = setInterval(() => {
+            getMessages();
+        }, 500);
+
+        if (currentGroup.id == null) {
+            setOpenDialog(true);
+        }
+
+        return () => clearInterval(interval)
+    }, [messagesLimit]);
+
     return (
-        <div className="Chat-page">
-            chat stuff here
-        </div>
+        <ChatPageBody>
+            <Dialog open={openDialog}>
+                <BoldDialogTitle>
+                    No Group Selected
+                </BoldDialogTitle>
+
+                <DialogContent>
+                    Please select a group fom the Groups Page before accessing the Chat.
+                </DialogContent>
+
+                <DialogActions>
+                    <Button onClick={() => { 
+                        setOpenDialog(false);
+                        history.push('/api/groups');
+                    }}>
+                        <BoldDialogAction>
+                            Close
+                        </BoldDialogAction>
+                    </Button>
+                </DialogActions>
+            </Dialog>
+
+            <LoadMoreButton onClick={() => {
+                setMessagesLimit(messagesLimit + 20);
+            }}>
+                Load More
+            </LoadMoreButton>
+
+            <ChatMessagesArea>
+                <ChatFeed 
+                    messages={ messages }
+                    showSenderName 
+                    bubblesCentered={ true }
+                    bubbleStyles={
+                        {
+                            text: {
+                                fontSize: 15,
+                                color: 'black'
+                            },
+                            chatbubble: {
+                                borderRadius: 75,
+                                padding: 15
+                            }
+                        }
+                    }
+                />
+            </ChatMessagesArea>
+
+            <InputArea>
+                <MessageInput 
+                    variant='outlined'
+                    placeholder='Type a message...'
+                    multiline={ true }
+                    onChange={(event) => {
+                        setComposedMessage(event.target.value);
+                    }}
+                    value={ composedMessage }
+                />
+                <Button 
+                    variant='contained' 
+                    endIcon={ <SendIcon /> }
+                    onClick={() => {
+                        handleMessageSent();
+                        setComposedMessage('');
+                    }}
+                >
+                    Send
+                </Button>
+            </InputArea>
+        </ChatPageBody>
     );
 }
+
+const ChatMessagesArea = styled(Box)({
+    marginBottom: '60px',
+    overflow: 'auto',
+    height: '100%'
+});
+
+const ChatPageBody = styled(Box)({
+    backgroundColor: 'black',
+    height: '75%',
+    padding: '20px',
+    display: 'flex',
+    flexDirection: 'column'
+});
+
+const MessageInput = styled(TextField)({
+    backgroundColor: 'white',
+    borderRadius: '5px',
+    flex: '5',
+});
+
+const InputArea = styled(Box)({ // Make this scrollable
+    position: 'fixed',
+    bottom: '20px',
+    right: '0px',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+});
+
+const LoadMoreButton = styled(Button)({
+    color: 'white'
+});
+
+const BoldDialogAction = styled(Typography)({
+    fontWeight: 'bold'
+});
+
+const BoldDialogTitle = styled(Typography)({
+    fontSize: '30px',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: '20px'
+});
