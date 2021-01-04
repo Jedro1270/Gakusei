d011ab5d4d72f82a95eea3fd216d4a9386cafebf
Jedro1270 <jedro1270@gmail.com>
2021-01-04 15:18:44 +0800

    feat: create member ranking component

---
+++ client/src/Pages/Main App/Body/Rankings/MemberRanking.js
@@ -0,0 +1,50 @@
+import { Box, styled, Typography } from '@material-ui/core';
+import React from 'react';
+
+export default function MemberRanking(props) {
+
+    return (
+        <MemberRankingBody>
+            <RankingNumber>
+                {props.rankingNumber}.
+            </RankingNumber>
+
+            <MemberDetailsSection>
+                <MemberUserName>
+                    {props.username}
+                </MemberUserName>
+                <MemberLevel>
+                    Level {props.level} ({props.points} points)
+                </MemberLevel>
+            </MemberDetailsSection>
+        </MemberRankingBody>
+    );
+}
+
+const MemberRankingBody = styled(Box)({
+    display: 'flex',
+    flexDirection: 'row',
+    alignSelf: 'center'
+});
+
+const RankingNumber = styled(Typography)({
+    color: 'white',
+    margin: '30px',
+    fontSize: '20px',
+    fontWeight: 'bold'
+});
+
+const MemberDetailsSection = styled(Box)({
+    display: 'flex',
+    flexDirection: 'column',
+    margin: '30px'
+});
+
+const MemberUserName = styled(Typography)({
+    color: 'white',
+    fontSize: '20px'
+});
+
+const MemberLevel = styled(Typography)({
+    color: 'green'
+});
