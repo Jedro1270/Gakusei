f4dc267f198a2848c25d8f1dd80f366f3f41ab6b
Jedro1270 <jedro1270@gmail.com>
2021-01-04 15:11:43 +0800

    feat: add data for badges table

--- seeds/initial_data.js
+++ seeds/initial_data.js
@@ -14,6 +14,18 @@ exports.seed = function(knex) {
           (2800, 3599),
           (3600, 4499),
           (4500, 5499);
+
+      INSERT INTO "badges"(badge_type, badge_icon, badge_name, badge_description, points_value)
+        VALUES
+          ('MILESTONE', 'pomo-peasant-badge.png', 'Pomo Peasant', 'Successfuly Finish 2 Pomodoro Timers', 100),
+          ('MILESTONE', 'pomo-samurai-badge.jpg', 'Pomo Samurai', 'Successfuly Finish 8 Pomodoro Timers', 200),
+          ('MILESTONE', 'pomo-daimyo-badge.gif', 'Pomo Daimyo', 'Successfuly Finish 16 Pomodoro Timers', 400),
+          ('MILESTONE', 'pomo-daimyo-badge.png', 'Pomo Shogun', 'Successfuly Finish 32 Pomodoro Timers', 800),
+          ('MILESTONE', 'scholar-badge.png', 'Scholar', 'Create your First Note', 50),
+          ('MILESTONE', 'philosopher-badge.png', 'Philosopher', 'Create 5 Notes', 200),
+          ('MILESTONE', 'sage-badge.png', 'Sage', 'Create 15 Notes', 500),
+          ('SPECIAL', 'sensei-badge.gif', 'Sensei', 'Create a Group', 200),
+          ('SPECIAL', 'soke-badge.png', 'Soke', 'Become the Highest Ranking Member of your Group', 400);
     `
   );
 };
