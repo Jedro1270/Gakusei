7f7204f7d3b4adf203b8fd3fead22b71891ad87c
Jedro1270 <jedro1270@gmail.com>
2021-01-04 15:14:11 +0800

    refactor: change note contents import path

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,10 @@ import Notebooks from './Pages/Main App/Body/Notebooks/Notebooks';
 import Chat from './Pages/Main App/Body/Chat/Chat';
 import Rankings from './Pages/Main App/Body/Rankings/Rankings';
 import Notes from './Pages/Main App/Body/Notebooks/Notes/Notes';
-import NoteContents from './Pages/Main App/Body/Notebooks/Notes/NoteContents/NoteContents';
+import NoteContents from './Pages/Main App/Body/Notebooks/Notes/Note Contents/NoteContents';
 import SignUp from './Pages/Log In/SignUp/SignUp';
 import SignIn from './Pages/Log In/SignIn/SignIn';
 import JoinGroup from './Pages/Main App/Body/Groups/Join Group/JoinGroup';
-import CreateGroup from './Pages/Main App/Body/Groups/Create Group/CreateGroup';
 
 export default function App() {
 
