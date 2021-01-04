8e16e3f27cb707de1c5f9e4ab31d0b638115d2a8
Jedro1270 <jedro1270@gmail.com>
2021-01-04 17:56:39 +0800

    refactor: move sign in and sign up logic to users route

--- server/Routes/users.js
+++ server/Routes/users.js
@@ -1,4 +1,91 @@
-export default function usersRoutes(app, secureRoute, upload, database) {
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+export default function usersRoutes(app, secureRoute, upload, database, passport) {
+
+    // Sign In
+    app.post('/sign-in', (request, response, next) => {
+      passport.authenticate('local', (error, user) => {
+        if (error) {
+          console.log(`ERROR: ${error}`)
+        } else if (!user) {
+          console.log('No User exists');
+          response.json({ message: 'Invalid Username or Password' });
+        } else {
+          request.logIn(user, { session: false }, (error) => {
+            if (error) {
+              console.log(`ERROR: ${error}`);
+              return next(error);
+            }
+
+            const body = {
+              id: user.user_id,
+              username: user.username,
+              profilePicture: user.profile_picture,
+              points: user.points,
+              level: user.level_id,
+              levelPointsMax: user.maximum_points,
+              levelPointsMin: user.minimum_points
+            }
+
+            const token = jwt.sign({ user: body }, process.env.TOKEN_SECRET)
+
+            response.json({
+              message: 'Successfully Authenticated',
+              token: token,
+              user: body
+            });
+          });
+        }
+      })(request, response, next);
+    });
+
+    // Sign Up
+    app.post('/sign-up', async (request, response) => {
+      const hashedPassword = await bcrypt.hash(request.body.password, 10);
+
+      database.query(
+        `
+          INSERT INTO "users"(username, password, points)
+            VALUES($1, $2, 0)
+            ON CONFLICT (username) DO NOTHING
+          RETURNING *;
+        `, [request.body.username, hashedPassword],
+        (error, results) => {
+          if (error) {
+            console.log(`ERROR: ${error}`);
+          } else {
+
+            if (results.rows.length === 0) {
+
+              console.log('Username taken');
+              response.json({ message: 'Username Taken' });
+
+            } else {
+
+              const user = results.rows[0];
+
+              database.query(
+                `
+                  INSERT INTO "level_achievements"(user_id, level_id)
+                    VALUES($1, $2)
+                  RETURNING *;
+                `, [user.user_id, 1],
+                (error, results) => {
+                  if (error) {
+                    console.log(`ERROR: ${error}`);
+                  } else {
+                      console.log('User inserted');
+                      response.json({ message: 'User Inserted' });
+                  }
+                }
+              );
+
+            }
+          }
+        }
+      );
+    });
 
     // Update user profile picture
     app.put('/api/users', secureRoute, upload.single('file'), (request, response) => {
--- server/server.js
+++ server/server.js
@@ -4,8 +4,6 @@ import express from 'express';
 import multer from 'multer';
 import cors from 'cors';
 import passport from 'passport';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 
 import passportStrategy from './passportConfig.js';
@@ -78,88 +76,6 @@ pool.connect((error, client) => {
 
     passportStrategy(passport, database);
 
-    // Sign In
-    app.post('/sign-in', (request, response, next) => {
-      passport.authenticate('local', (error, user) => {
-        if (error) {
-          console.log(`ERROR: ${error}`)
-        } else if (!user) {
-          console.log('No User exists');
-          response.json({ message: 'Invalid Username or Password' });
-        } else {
-          request.logIn(user, { session: false }, (error) => {
-            if (error) {
-              console.log(`ERROR: ${error}`);
-              return next(error);
-            }
-
-            const body = {
-              id: user.user_id,
-              username: user.username,
-              profilePicture: user.profile_picture,
-              points: user.points,
-              level: user.level_id
-            }
-
-            const token = jwt.sign({ user: body }, process.env.TOKEN_SECRET)
-
-            response.json({
-              message: 'Successfully Authenticated',
-              token: token,
-              user: body
-            });
-          });
-        }
-      })(request, response, next);
-    });
-
-    // Sign Up
-    app.post('/sign-up', async (request, response) => {
-      const hashedPassword = await bcrypt.hash(request.body.password, 10);
-
-      database.query(
-        `
-          INSERT INTO "users"(username, password, points)
-            VALUES($1, $2, 0)
-            ON CONFLICT (username) DO NOTHING
-          RETURNING *;
-        `, [request.body.username, hashedPassword],
-        (error, results) => {
-          if (error) {
-            console.log(`ERROR: ${error}`);
-          } else {
-
-            if (results.rows.length === 0) {
-
-              console.log('Username taken');
-              response.json({ message: 'Username Taken' });
-
-            } else {
-
-              const user = results.rows[0];
-
-              database.query(
-                `
-                  INSERT INTO "level_achievements"(user_id, level_id)
-                    VALUES($1, $2)
-                  RETURNING *;
-                `, [user.user_id, 1],
-                (error, results) => {
-                  if (error) {
-                    console.log(`ERROR: ${error}`);
-                  } else {
-                      console.log('User inserted');
-                      response.json({ message: 'User Inserted' });
-                  }
-                }
-              );
-
-            }
-          }
-        }
-      );
-    });
-
     const secureRoute = (request, response, next) => {
       passport.authenticate('jwt', { session: false }, (error, user) => {
         if (error) {
@@ -182,7 +98,7 @@ pool.connect((error, client) => {
     groupsRoutes(app, secureRoute, uploadGroupImage, database);
     notebooksRoutes(app, secureRoute, database);
     pomodoroRoutes(app, secureRoute, database);
-    usersRoutes(app, secureRoute, uploadProfilePicture, database);
+    usersRoutes(app, secureRoute, uploadProfilePicture, database, passport);
     chatRoutes(app, secureRoute, database);
     rankingsRoutes(app, secureRoute, database);
     badgesRoutes(app, secureRoute, database);
