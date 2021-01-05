75a6831e8c14f846854379502e5968520f846455
Jedro1270 <jedro1270@gmail.com>
2021-01-05 10:56:06 +0800

    refactor: factor out openSnackbar and setOpenSnackbar to props

--- client/src/Pages/Main App/Body/Snackbar Notifications/BadgeEarned.js
+++ client/src/Pages/Main App/Body/Snackbar Notifications/BadgeEarned.js
@@ -1,14 +1,12 @@
 import { Snackbar } from '@material-ui/core';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 export default function BadgeEarned(props) {
-    const [openSnackbar, setOpenSnackbar] = useState(props.open);
-
     return (
         <Snackbar
             anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-            open={openSnackbar}
-            onClose={() => {setOpenSnackbar(false)}}
+            open={props.openSnackbar}
+            onClose={() => {props.setOpenSnackbar(false)}}
             message={`You have earned the ${props.badgeTitle} badge!`}
             autoHideDuration={6000}
         />
