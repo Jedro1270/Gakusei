d2d511e04edad24d0eb5537c2c6960a3bbc9cdc1
Jedro1270 <jedro1270@gmail.com>
2021-01-06 10:08:09 +0800

    refactor: replace replaceAll with split and join

--- client/src/Pages/Main App/Helper Functions/createTitle.js
+++ client/src/Pages/Main App/Helper Functions/createTitle.js
@@ -2,8 +2,7 @@ export default function createTitle(name) {
     const excludedWords = ['the', 'and', 'with', 'from'];
 
     return name
-        .replaceAll('-', ' ')
-        .split(' ')
+        .split('-')
         .map((word, index) => {
             if ((word.length > 2 && !excludedWords.includes(word)) || index === 0) {
                 const firstLetter = word[0].toUpperCase();
--- client/src/Pages/Main App/Helper Functions/createURL.js
+++ client/src/Pages/Main App/Helper Functions/createURL.js
@@ -1,3 +1,3 @@
 export default function createURL(name) {
-    return name.replaceAll(' ', '-').toLowerCase();
+    return name.split(' ').join('-').toLowerCase();
 }
