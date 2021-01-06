8334c2e31c7a058719fb60dad3daa14ae0832415
Jedro1270 <jedro1270@gmail.com>
2021-01-06 09:49:02 +0800

    fix: fix certain propositions being capitalized

--- client/src/Pages/Main App/Helper Functions/createTitle.js
+++ client/src/Pages/Main App/Helper Functions/createTitle.js
@@ -1,10 +1,16 @@
 export default function createTitle(name) {
+    const excludedWords = ['the', 'and', 'with', 'from'];
+
     return name
         .replaceAll('-', ' ')
         .split(' ')
-        .map((word) => {
-            const firstLetter = word[0].toUpperCase();
-            return `${firstLetter}${word.slice(1)}`
+        .map((word, index) => {
+            if ((word.length > 2 && !excludedWords.includes(word)) || index === 0) {
+                const firstLetter = word[0].toUpperCase();
+                return `${firstLetter}${word.slice(1)}`
+            } 
+
+            return word;
         })
         .join(' ');
 }
