9783dafc8cc13eef5ebc7918587f291d4f5a807a
Jedro1270 <jedro1270@gmail.com>
2021-01-08 16:50:01 +0800

    feat: create bold dialog title component

---
+++ client/src/Pages/Main App/Body/Error Dialogs/Dialog Components/BoldDialogTitle.js
@@ -0,0 +1,17 @@
+import { styled, Typography } from '@material-ui/core';
+
+export default function BoldDialogTitle(props) {
+
+    return (
+        <BoldDialogTitleBody>
+            {props.content}
+        </BoldDialogTitleBody>
+    );
+}
+
+const BoldDialogTitleBody = styled(Typography)({
+    fontSize: '30px',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: '20px'
+});
