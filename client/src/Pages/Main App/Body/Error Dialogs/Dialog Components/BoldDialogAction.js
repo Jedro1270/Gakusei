711659cf3952bd83666a2e3e69a9e40525c68971
Jedro1270 <jedro1270@gmail.com>
2021-01-08 16:49:42 +0800

    feat: create bold dialog action component

---
+++ client/src/Pages/Main App/Body/Error Dialogs/Dialog Components/BoldDialogAction.js
@@ -0,0 +1,14 @@
+import { styled, Typography } from '@material-ui/core';
+
+export default function BoldDialogAction(props) {
+    return (
+        <BoldDialogActionBody>
+            {props.content}
+        </BoldDialogActionBody>
+    )
+    
+}
+
+const BoldDialogActionBody = styled(Typography)({
+    fontWeight: 'bold'
+});
